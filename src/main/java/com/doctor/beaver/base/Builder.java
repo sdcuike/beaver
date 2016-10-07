f465d63b00c4c20bd529a6e935582f951b4f26d9
sdcuke <sdcuike@aol.com>
2016-10-07 19:13:15 +0800

    add Builder pattern 接口

---
+++ src/main/java/com/doctor/beaver/base/Builder.java
@@ -0,0 +1,13 @@
+package com.doctor.beaver.base;
+
+/**
+ * @author sdcuike
+ *         <p>
+ *         Created on 2016年10月7日
+ *         <p>
+ *         Builder pattern 接口；代码来源于Effective.Java(2nd,2008.5)].Joshua.Bloch
+ */
+public interface Builder<T> {
+
+    T build();
+}
